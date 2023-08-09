5109a12f71d94442f69fe18ec513bd9e77b3ca76
Rakshit Arora <rakshit087@gmail.com>
2023-08-09 19:56:04 +0530

    navbar for lager screens

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -1,8 +1,8 @@
 <nav class="flex text-white justify-between items-center">
   <p class="text-2xl md:text-4xl">🌜</p>
   <div class="text-sm md:text-lg">
-    <a href="/" class="mr-4">About</a>
-    <a href="/" class="mr-4">Features</a>
+    <a href="/" class="mr-4 lg:mr-8">About</a>
+    <a href="/" class="mr-4 lg:mr-8">Features</a>
     <a href="/">FAQ</a>
   </div>
 </nav>
