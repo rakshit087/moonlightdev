e501c9caefa91d584032f90164725247fadd4917
Rakshit Arora <rakshit087@gmail.com>
2023-08-09 19:50:50 +0530

    navbar for mobile

---
+++ src/lib/components/navbar.svelte
@@ -0,0 +1,8 @@
+<nav class="flex text-white justify-between items-center">
+  <p class="text-2xl">🌜</p>
+  <div>
+    <a href="/" class="mr-4 text-sm">About</a>
+    <a href="/" class="mr-4 text-sm">Features</a>
+    <a href="/" class="text-sm">FAQ</a>
+  </div>
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,11 @@
 <script>
+  import Navbar from '$lib/components/navbar.svelte';
   import '../global.css';
 </script>
 
-<main class="h-screen overflow-y-scroll scroll-smooth snap-mandatory snap-y bg-backdrop">
+<main
+  class="h-screen overflow-y-scroll scroll-smooth snap-mandatory snap-y bg-backdrop p-8 md:px-24"
+>
+  <Navbar />
   <slot />
 </main>
