3399911d4c4433632ea80a799e035490d6a31895
Rakshit Arora <rakshit087@gmail.com>
2023-08-09 23:36:27 +0530

    added responsiveness

--- src/lib/components/button.svelte
+++ src/lib/components/button.svelte
@@ -3,4 +3,8 @@
   export let customClasses: string = '';
 </script>
 
-<button class="bg-yellow-50 py-2 px-8 rounded-lg text-backdrop {customClasses}">{label}</button>
+<button
+  class="bg-yellow-50 py-2 text-sm md:text-base px-6 md:px-8 rounded-lg text-backdrop {customClasses}"
+>
+  {label}
+</button>
--- src/lib/components/home.svelte
+++ src/lib/components/home.svelte
@@ -5,8 +5,8 @@
 <div
   class="h-full flex justify-center items-center flex-col snap-start snap-always bg-backdrop p-8 md:px-24 text-white"
 >
-  <h1 class="text-8xl font-display">moonlight</h1>
-  <h1 class="text-8xl font-display">devs</h1>
+  <h1 class="lg:text-8xl md:text-6xl font-display text-4xl">moonlight</h1>
+  <h1 class="lg:text-8xl md:text-6xl font-display text-4xl">devs</h1>
   <div class="flex mt-16">
     <Button label="i am a dev" customClasses="mr-4" />
     <Button label="i am an org" customClasses="ml-4" />
