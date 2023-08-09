0550d83f549a5998eb7015631e17e78b462d38dd
Rakshit Arora <rakshit087@gmail.com>
2023-08-09 23:26:47 +0530

    added primary button

---
+++ src/lib/components/button.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+  export let label: string;
+  export let customClasses: string = '';
+</script>
+
+<button class="bg-yellow-50 py-2 px-8 rounded-lg text-backdrop {customClasses}">{label}</button>
