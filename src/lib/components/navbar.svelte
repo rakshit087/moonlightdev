09699eaabbd1516d86766ba3d1db094c1d2092db
Rakshit Arora <rakshit087@gmail.com>
2023-08-10 22:40:03 +0530

    added moon icon

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -1,7 +1,11 @@
+<script>
+  import Moon from '$lib/icons/moon.svelte';
+</script>
+
 <nav
-  class="fixed right-4 left-4 top-8 md:right-20 md:left-20 lg:right-24 lg:left-24 z-50 flex justify-between items-center px-4 py-2 border-2 rounded-2xl backdrop-blur-md"
+  class="fixed right-4 left-4 top-8 md:right-20 md:left-20 lg:right-24 lg:left-24 z-50 flex justify-between items-center px-4 py-2 border-2 rounded-2xl backdrop-blur-md mix-blend-difference"
 >
-  <p class="text-2xl md:text-4xl">🌜</p>
+  <Moon class="text-white text-3xl" />
   <div class="text-sm md:text-lg text-white">
     <a href="/" class="mr-4 lg:mr-8">About</a>
     <a href="/" class="mr-4 lg:mr-8">Features</a>
