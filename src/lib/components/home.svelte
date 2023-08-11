5a78c0565aa5a79e85ee94d5528ecf811eb9f1e7
Rakshit Arora <rakshit087@gmail.com>
2023-08-11 20:10:26 +0530

    changed call to action buttons

--- src/lib/components/home.svelte
+++ src/lib/components/home.svelte
@@ -9,10 +9,10 @@
   <h1 class="lg:text-8xl md:text-6xl font-display text-4xl">devs</h1>
   <div class="flex mt-16">
     <a href="#features" class="mr-4">
-      <Button label="i am a dev" customClasses="mr-4" />
+      <Button label="find a job" />
     </a>
-    <a href="#features" class="mr-4">
-      <Button label="i am an org" customClasses="ml-4" />
+    <a href="#features" class="ml-4">
+      <Button label="post a job" />
     </a>
   </div>
 </div>
