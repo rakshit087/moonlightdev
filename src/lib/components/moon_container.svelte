e89ae9058cafdbee5953f919c0ab871aedfc6962
Rakshit Arora <rakshit087@gmail.com>
2023-08-13 11:33:31 +0530

    moon container

---
+++ src/lib/components/moon_container.svelte
@@ -0,0 +1,17 @@
+<div class="moon py-8">
+  <slot />
+</div>
+
+<style>
+  .moon {
+    background-image: url('$lib/assets/moon_texture.svg');
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-color: white;
+    clip-path: ellipse(120% 100% at 50% 100%);
+    display: flex;
+    flex-direction: column;
+    margin: 3rem 0;
+  }
+</style>
