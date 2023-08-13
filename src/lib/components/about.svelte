a4886559979e9b3c6f2f889503a33dd0d1171c31
Rakshit Arora <rakshit087@gmail.com>
2023-08-13 14:52:08 +0530

    for devs section

--- src/lib/components/about.svelte
+++ src/lib/components/about.svelte
@@ -1,3 +1,27 @@
-<p class="text-4xl font-semibold">
-  find part time <br /> moonlighting opportunities
-</p>
+<script>
+  import StepCard from './StepCard.svelte';
+</script>
+
+<div id="devs" class="h-screen flex flex-col justify-center">
+  <p class="text-4xl font-semibold">Find awesome <br /> moonlighting opportunities</p>
+  <p class="mt-4 text-xl">
+    We help you find long-term part time contract based work to earn some side income.
+  </p>
+  <div class="grid grid-cols-1 lg:grid-cols-3 gap-4 mt-16">
+    <StepCard
+      step="1"
+      title="Make a profile"
+      description="Create a profile with your skills, experience, and availability. We'll match you with opportunities that fit your profile."
+    />
+    <StepCard
+      step="2"
+      title="Search and apply"
+      description="Search by your favorite language, framework, or tool. Write a small proposal and apply for the job."
+    />
+    <StepCard
+      step="3"
+      title="Get hired 🚀"
+      description="If the client likes your proposal, they'll reach out to you. You can negotiate the terms and get started."
+    />
+  </div>
+</div>
