e7d6e12f6f3a80848a8029dec89463a3014388c4
Rakshit Arora <rakshit087@gmail.com>
2023-08-13 12:56:14 +0530

    made waitlist screen

--- src/lib/components/waitlist.svelte
+++ src/lib/components/waitlist.svelte
@@ -1,21 +1,23 @@
-<div
-  class="flex flex-col mx-auto w-full max-w-2xl border-2 rounded-xl p-6 md:p-8 border-black backdrop-blur-sm"
->
-  <p class="md:text-5xl mb-8 text-4xl">we will be launching pretty soon...</p>
-  <p class="text-xl md:text-2xl">join our waitlist to get an early access to our beta!</p>
+<div class="h-screen flex items-center" id="waitlist">
+  <div
+    class="flex flex-col mx-auto w-full max-w-2xl border-2 rounded-xl p-6 md:p-8 border-black backdrop-blur-sm"
+  >
+    <p class="md:text-5xl mb-8 text-4xl">we will be launching pretty soon...</p>
+    <p class="text-xl md:text-2xl">join our waitlist to get an early access to our beta!</p>
 
-  <p class="mt-8 md:mt-12">where can we contact you?</p>
-  <input
-    type="email"
-    placeholder="anon@moonlightdevs.xyz"
-    class="border-2 border-black rounded-lg p-2 mt-1"
-  />
-  <p class="md:mt-6 mt-4">you want to...</p>
-  <select class="border-2 border-black rounded-lg p-2 mt-1">
-    <option value="dev">find part time jobs</option>
-    <option value="org">hire devs</option>
-  </select>
-  <button class="bg-black text-white rounded-lg p-2 mt-8 md:mt-10 w-full focus:border-black">
-    join waitlist
-  </button>
+    <p class="mt-8 md:mt-12">where can we contact you?</p>
+    <input
+      type="email"
+      placeholder="anon@moonlightdevs.xyz"
+      class="border-2 border-black rounded-lg p-2 mt-1"
+    />
+    <p class="md:mt-6 mt-4">you want to...</p>
+    <select class="border-2 border-black rounded-lg p-2 mt-1">
+      <option value="dev">find part time jobs</option>
+      <option value="org">hire devs</option>
+    </select>
+    <button class="bg-black text-white rounded-lg p-2 mt-8 md:mt-10 w-full focus:border-black">
+      join waitlist
+    </button>
+  </div>
 </div>
