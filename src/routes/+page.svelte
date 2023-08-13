251b88cfedd45812bbdbe1e3326e7a6bf616c90b
Rakshit Arora <rakshit087@gmail.com>
2023-08-13 11:33:38 +0530

    final layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,13 @@
   import About from '$lib/components/about.svelte';
   import Home from '$lib/components/home.svelte';
   import Waitlist from '$lib/components/waitlist.svelte';
+  import JoinUs from '$lib/components/join_us.svelte';
+  import MoonContainer from '$lib/components/moon_container.svelte';
 </script>
 
 <Home />
-<About />
-<Waitlist />
+<MoonContainer>
+  <JoinUs />
+  <About />
+  <Waitlist />
+</MoonContainer>
