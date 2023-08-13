c4987240b1adc3731698073bfb58a6c4eea9b6a7
Rakshit Arora <rakshit087@gmail.com>
2023-08-13 11:44:06 +0530

    clip path responsive

--- src/lib/components/moon_container.svelte
+++ src/lib/components/moon_container.svelte
@@ -1,4 +1,4 @@
-<div class="moon py-8">
+<div class="moon py-8 px-4 md:px-20 lg:px-24">
   <slot />
 </div>
 
@@ -9,9 +9,21 @@
     background-position: center;
     background-repeat: no-repeat;
     background-color: white;
-    clip-path: ellipse(120% 100% at 50% 100%);
+    clip-path: ellipse(250% 100% at 50% 100%);
     display: flex;
     flex-direction: column;
     margin: 3rem 0;
+  }
+
+  @media (min-width: 768px) {
+    .moon {
+      clip-path: ellipse(185% 100% at 50% 100%);
+    }
+  }
+
+  @media (min-width: 1024px) {
+    .moon {
+      clip-path: ellipse(120% 100% at 50% 100%);
+    }
   }
 </style>
