65f3e439b58d8b1ba564cc5adc7af5ea233329c5
Rakshit Arora <rakshit087@gmail.com>
2023-08-13 11:44:30 +0530

    centered join us

--- src/lib/components/join_us.svelte
+++ src/lib/components/join_us.svelte
@@ -2,7 +2,7 @@
   import Button from './button.svelte';
 </script>
 
-<div class="flex h-[50vh]">
+<div class="flex h-[50vh] mx-auto">
   <Button label="find a job" />
   <Button label="post a job" />
 </div>
