de62ab63fc9074eeee0bf4b14e0e0e862dd15960
Rakshit Arora <rakshit087@gmail.com>
2023-08-13 11:23:36 +0530

    cleaned about section

--- src/lib/components/about.svelte
+++ src/lib/components/about.svelte
@@ -1,8 +1,3 @@
-<div
-  class="h-full flex justify-center flex-col bg-white snap-start snap-always p-8 md:px-24"
-  id="features"
->
-  <p class="text-4xl font-semibold">
-    find part time <br /> moonlighting opportunities
-  </p>
-</div>
+<p class="text-4xl font-semibold">
+  find part time <br /> moonlighting opportunities
+</p>
