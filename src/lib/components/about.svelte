7f9c46e8e27be8f32a12e7fff91aa50478508924
Rakshit Arora <rakshit087@gmail.com>
2023-08-14 00:50:14 +0530

    updated about

--- src/lib/components/about.svelte
+++ src/lib/components/about.svelte
@@ -11,24 +11,39 @@
   {:else}
     <Typewriter customClasses="text-4xl font-semibold" text="developers for your project" />
   {/if}
-  <p class="mt-4 text-xl">
-    We help you find long-term part time contract based work to earn some side income.
-  </p>
   <div class="grid grid-cols-1 lg:grid-cols-3 gap-4 mt-16">
-    <StepCard
-      step="1"
-      title="Make a profile"
-      description="Create a profile with your skills, experience, and availability. We'll match you with opportunities that fit your profile."
-    />
-    <StepCard
-      step="2"
-      title="Search and apply"
-      description="Search by your favorite language, framework, or tool. Write a small proposal and apply for the job."
-    />
-    <StepCard
-      step="3"
-      title="Get hired 🚀"
-      description="If the client likes your proposal, they'll reach out to you. You can negotiate the terms and get started."
-    />
+    {#if $userType === UserType.DEVELOPER}
+      <StepCard
+        step="1"
+        title="Make a profile"
+        description="Create a profile with your skills, experience, and availability. We'll match you with opportunities that fit your profile."
+      />
+      <StepCard
+        step="2"
+        title="Search and apply"
+        description="Search by your favorite language, framework, or tool. Write a small proposal and apply for the job."
+      />
+      <StepCard
+        step="3"
+        title="Get hired 🚀"
+        description="If the client likes your proposal, they'll reach out to you. You can negotiate the terms and get started."
+      />
+    {:else}
+      <StepCard
+        step="1"
+        title="Post a Job"
+        description="Enter details, requirements, and budget for your project and wait for developers to apply."
+      />
+      <StepCard
+        step="2"
+        title="Browse applicants"
+        description="Browse through the applicants and their proposals. You can also search by language, framework, or tool."
+      />
+      <StepCard
+        step="3"
+        title="Hire a developer"
+        description="Once you find the right developer, you can negotiate the terms and get started."
+      />
+    {/if}
   </div>
 </div>
