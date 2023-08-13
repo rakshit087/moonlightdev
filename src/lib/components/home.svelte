a39c6c43bef6d3a1f0b92bc7a643f2f9ba713cc0
Rakshit Arora <rakshit087@gmail.com>
2023-08-13 19:05:25 +0530

    added swap button

--- src/lib/components/home.svelte
+++ src/lib/components/home.svelte
@@ -1,12 +1,7 @@
-<script lang="ts">
-  import Typewriter from 'svelte-typewriter';
-</script>
-
 <div class="h-[60vh] flex flex-col justify-end snap-start snap-always">
   <h1 class="font-display text-white text-center text-4xl md:text-7xl lg:text-7xl mb-8">
     moonlight <br /> devs
   </h1>
-  <Typewriter cursor>
-    <p class="text-white text-center text-lg md:text-3xl mb-24">find your next side hustle!</p>
-  </Typewriter>
+
+  <p class="text-white text-center text-lg md:text-3xl mb-24">find your next side hustle!</p>
 </div>
--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -1,13 +1,18 @@
 <script>
   import Moon from '$lib/assets/icons/moon.svelte';
+  import SwapIcon from '$lib/assets/icons/swap_icon.svelte';
 </script>
 
 <nav
-  class="fixed right-4 left-4 top-8 md:right-20 md:left-20 lg:right-24 lg:left-24 z-50 flex justify-between items-center px-4 py-2 border-2 rounded-2xl backdrop-blur-md mix-blend-difference"
+  class="fixed right-4 left-4 top-8 py-2 px-4 md:right-20 md:left-20 lg:right-24 lg:left-24 z-50 flex justify-between items-center border-2 rounded-2xl backdrop-blur-md mix-blend-difference"
 >
   <Moon class="text-white text-3xl" />
-  <div class="text-sm md:text-lg text-white">
+  <div class="text-sm md:text-lg text-white flex items-center absolute right-1/2 translate-x-1/2">
     <a href="#about" class="mr-4 lg:mr-8">About</a>
-    <a href="#waitlist" class="mr-4 lg:mr-8">Waitlist</a>
+    <a href="#waitlist">Waitlist</a>
+  </div>
+  <div class="flex items-center text-white">
+    <p class="text-sm md:text-lg mr-4 lg:mr-8 hidden md:block">Developer</p>
+    <SwapIcon class="text-3xl" />
   </div>
 </nav>
