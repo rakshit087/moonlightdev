27bac073bd7f3656776fca17a3be6bae1a591cb8
Rakshit Arora <rakshit087@gmail.com>
2023-08-13 21:17:12 +0530

    added typewriter animations

--- src/lib/components/home.svelte
+++ src/lib/components/home.svelte
@@ -1,7 +1,23 @@
+<script>
+  import { UserType, userType } from '$lib/store';
+  import Typewriter from './typewriter.svelte';
+</script>
+
 <div class="h-[60vh] flex flex-col justify-end snap-start snap-always">
   <h1 class="font-display text-white text-center text-4xl md:text-7xl lg:text-7xl mb-8">
     moonlight <br /> devs
   </h1>
-
-  <p class="text-white text-center text-lg md:text-3xl mb-24">find your next side hustle!</p>
+  {#if $userType === UserType.DEVELOPER}
+    <Typewriter>
+      <p class="text-white text-center text-lg md:text-3xl mb-24">
+        A platform for developers to find their dream job.
+      </p>
+    </Typewriter>
+  {:else}
+    <Typewriter>
+      <p class="text-white text-center text-lg md:text-3xl mb-24">
+        A platform for developers to find their dream developer.
+      </p>
+    </Typewriter>
+  {/if}
 </div>
---
+++ src/lib/components/typewriter.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { disableAnimations } from '$lib/store';
+  import Typewriter from 'svelte-typewriter';
+</script>
+
+{#if $disableAnimations}
+  <slot />
+{:else}
+  <Typewriter mode="scramble" scrambleDuration={500}>
+    <slot />
+  </Typewriter>
+{/if}
