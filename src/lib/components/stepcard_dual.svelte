4788c2f821a0cec474873fe2e50d1b947b3dbcd7
Rakshit Arora <rakshit087@gmail.com>
2023-08-14 08:27:56 +0530

    added step card dual

---
+++ src/lib/components/stepcard_dual.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { UserType, userType } from '$lib/store';
+  import StepCard from './step_card.svelte';
+  export let step: string;
+  export let devTitle: string;
+  export let devDescription: string;
+  export let orgTitle: string;
+  export let orgDescription: string;
+</script>
+
+{#if $userType === UserType.DEVELOPER}
+  <StepCard {step} title={devTitle} description={devDescription} />
+{:else}
+  <StepCard {step} title={orgTitle} description={orgDescription} />
+{/if}
