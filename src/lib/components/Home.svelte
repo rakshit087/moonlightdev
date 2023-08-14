a9fa0bfb62dad4d04b608ea310781904da14c88d
Rakshit Arora <rakshit087@gmail.com>
2023-08-15 01:48:51 +0530

    renamed files

---
+++ src/lib/components/Home.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import TypewriterDual from './TypewriterDual.svelte';
+</script>
+
+<div class="h-[60vh] flex flex-col justify-end snap-start snap-always">
+  <h1 class="font-display text-white text-center text-4xl md:text-7xl lg:text-7xl mb-8">
+    moonlight <br /> devs
+  </h1>
+  <TypewriterDual
+    customClasses="text-white text-center text-lg md:text-3xl mb-24 mx-auto"
+    devText="find your next side hustle ✨"
+    orgText="find part time devs on demand ✨"
+  />
+</div>
---
+++ src/lib/components/Typewriter.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { disableAnimations } from '$lib/store';
+  import Typewriter from 'svelte-typewriter';
+
+  export let text: string;
+  export let customClasses: string;
+</script>
+
+{#if $disableAnimations}
+  <p class={customClasses}>
+    {text}
+  </p>
+{:else}
+  <div class="flex {customClasses}">
+    {text.slice(0, 2)}
+    <Typewriter mode="scramble" scrambleDuration={500} element="span">
+      {text.slice(2)}
+    </Typewriter>
+  </div>
+{/if}
