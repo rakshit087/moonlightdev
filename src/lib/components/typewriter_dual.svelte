c1c624b054f3d3bd86a5aee3fb6be60b437cedd4
Rakshit Arora <rakshit087@gmail.com>
2023-08-14 08:18:48 +0530

    added a typewriter dual element

---
+++ src/lib/components/typewriter_dual.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { UserType, userType } from '$lib/store';
+  import Typewriter from './typewriter.svelte';
+
+  export let devText: string;
+  export let orgText: string;
+  export let customClasses: string;
+</script>
+
+<Typewriter {customClasses} text={$userType === UserType.DEVELOPER ? devText : orgText} />
