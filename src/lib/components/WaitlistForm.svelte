31e27834264b0353a888ffa0e3835ecf3f4f30ed
Rakshit Arora <rakshit087@gmail.com>
2023-08-14 19:17:13 +0530

    seperate component for form

---
+++ src/lib/components/WaitlistForm.svelte
@@ -0,0 +1,23 @@
+<div
+  class="w-4/5 max-w-2xl border-2 rounded-xl p-6 md:p-8 border-black bg-white absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+>
+  <p class="md:text-5xl mb-4 text-3xl">we will be launching pretty soon...</p>
+  <p class="text-lg md:text-2xl">join our waitlist to get an early access to our beta!</p>
+
+  <p class="mt-8 md:mt-12">where can we contact you?</p>
+  <form>
+    <input
+      type="email"
+      placeholder="anon@moonlightdevs.xyz"
+      class="border-2 border-black rounded-lg p-2 mt-1 w-full"
+    />
+    <p class="md:mt-6 mt-4">you want to...</p>
+    <select class="border-2 border-black rounded-lg p-2 mt-1 w-full">
+      <option value="dev">find part time jobs</option>
+      <option value="org">hire devs</option>
+    </select>
+    <button class="bg-black text-white rounded-lg p-2 mt-8 md:mt-10 w-full focus:border-black">
+      join waitlist
+    </button>
+  </form>
+</div>
--- src/lib/components/waitlist.svelte
+++ src/lib/components/waitlist.svelte
@@ -1,26 +1,10 @@
+<script>
+  import WaitlistForm from './WaitlistForm.svelte';
+</script>
+
 <div class="h-screen snap-start snap-always relative" id="waitlist">
   <div class="moon h-1/3" />
-  <div
-    class="flex flex-col mx-auto w-4/5 max-w-2xl border-2 rounded-xl p-6 md:p-8 border-black bg-white backdrop-blur-sm absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
-  >
-    <p class="md:text-5xl mb-4 text-3xl">we will be launching pretty soon...</p>
-    <p class="text-lg md:text-2xl">join our waitlist to get an early access to our beta!</p>
-
-    <p class="mt-8 md:mt-12">where can we contact you?</p>
-    <input
-      type="email"
-      placeholder="anon@moonlightdevs.xyz"
-      class="border-2 border-black rounded-lg p-2 mt-1"
-    />
-    <p class="md:mt-6 mt-4">you want to...</p>
-    <select class="border-2 border-black rounded-lg p-2 mt-1">
-      <option value="dev">find part time jobs</option>
-      <option value="org">hire devs</option>
-    </select>
-    <button class="bg-black text-white rounded-lg p-2 mt-8 md:mt-10 w-full focus:border-black">
-      join waitlist
-    </button>
-  </div>
+  <WaitlistForm />
 </div>
 
 <style>
