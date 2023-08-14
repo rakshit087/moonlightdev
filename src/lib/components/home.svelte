c8776303c6e15c252e3683e655b86311e12477a9
Rakshit Arora <rakshit087@gmail.com>
2023-08-14 08:29:02 +0530

    refactored home

--- src/lib/components/home.svelte
+++ src/lib/components/home.svelte
@@ -1,21 +1,14 @@
-<script>
-  import { UserType, userType } from '$lib/store';
-  import Typewriter from './typewriter.svelte';
+<script lang="ts">
+  import TypewriterDual from './typewriter_dual.svelte';
 </script>
 
 <div class="h-[60vh] flex flex-col justify-end snap-start snap-always">
   <h1 class="font-display text-white text-center text-4xl md:text-7xl lg:text-7xl mb-8">
     moonlight <br /> devs
   </h1>
-  {#if $userType === UserType.DEVELOPER}
-    <Typewriter
-      customClasses="text-white text-center text-lg md:text-3xl mb-24 mx-auto"
-      text={'find your next side hustle ✨'}
-    />
-  {:else}
-    <Typewriter
-      customClasses="text-white text-center text-lg md:text-3xl mb-24 mx-auto"
-      text={'find part time devs on demand ✨'}
-    />
-  {/if}
+  <TypewriterDual
+    customClasses="text-white text-center text-lg md:text-3xl mb-24 mx-auto"
+    devText="find your next side hustle ✨"
+    orgText="find part time devs on demand ✨"
+  />
 </div>
