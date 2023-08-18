5fb9325ac019933eafbb5ad78acc9cbe6bfd0beb
Rakshit Arora <50898928+rakshit087@users.noreply.github.com>
2023-08-19 00:05:15 +0530

    removed duplicate files

--- src/lib/components/button.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-  export let label: string;
-</script>
-
-<button class="bg-black py-2 lg:text-xl px-6 md:px-8 lg:px-12 rounded-lg text-white mx-4">
-  <p class="text-sm md:text-xl">{label}</p>
-</button>
