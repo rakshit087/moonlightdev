75f7671743eb1401ef577a6d508e77e5fcc4d639
Rakshit Arora <rakshit087@gmail.com>
2023-08-19 20:49:38 +0530

    added meta data and description

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,14 @@
   import '../global.css';
 </script>
 
+<svelte:head>
+  <title>Moonlight Devs</title>
+  <meta
+    name="description"
+    content="find part time contract based work as a developer or hire proffesional developers on part time bases."
+  />
+</svelte:head>
+
 <main class="h-screen md:snap-y md:snap-mandatory overflow-y-scroll scroll-smooth bg-black">
   <Navbar />
   <slot />
